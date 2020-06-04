1dc517e479a141987bd48081f5c933df02ac7131
soulforger <n.zolbooyuro@gmail.com>
2020-06-04 13:27:44 +0800

    add: videojs

--- client/src/components/lessonvideo/lessonVideo.jsx
+++ client/src/components/lessonvideo/lessonVideo.jsx
@@ -2,13 +2,20 @@ import React from 'react'
 import './lessonVideo.scss'
 import Header from '../Header/Header'
 
-import lesson from "../assets/testing video/BLACKPINK - 'Kill This Love' M-V.webm"
+import lesson from "../assets/testing video/BLACKPINK - 'Kill This Love' M-V.mp4"
 
 const LessonVideo = () => {
   return (
     <div className='videoMain'>
       <Header />
-      <video className='video' id='player' playsInline controls>
+      <video
+        id='my-video'
+        class='video-js video'
+        controls
+        preload='auto'
+        poster='MY_VIDEO_POSTER.jpg'
+        data-setup='{"liveui": false}'
+      >
         <source src={lesson} type='video/mp4' />
         <source src={lesson} type='video/webm' />
       </video>
