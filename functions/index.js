89af13e8c946cd921b0eb31efc918e946258d795
Zolboo <zolboo@nest-41s-iMac.local>
2020-06-06 14:56:24 +0800

    feat: incoming changes number 0.2

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,13 @@
 const functions = require('firebase-functions');
-// const admin = require('firebase-admin');
-// admin.initializeApp();
+const admin = require('firebase-admin');
+admin.initializeApp();
 
+let db = admin.firestore();
+
+db.collection('users').get()
+    .then((res) => {
+        console.log(res)
+    })
 
 exports.app = functions.https.onRequest((req, res) => {
     res.send('hello from nomt')
