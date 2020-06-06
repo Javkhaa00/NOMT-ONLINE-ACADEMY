28ddfd359229fe53d3ca35a47aa67d467b2ea154
Zolboo <zolboo@nest-41s-iMac.local>
2020-06-06 14:21:20 +0800

    feat: incoming updated number 0.1

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,8 @@
 const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-admin.initializeApp();
+// const admin = require('firebase-admin');
+// admin.initializeApp();
 
 
-exports.app = functions.https.onRequest(app);
+exports.app = functions.https.onRequest((req, res) => {
+    res.send('hello from nomt')
+});
