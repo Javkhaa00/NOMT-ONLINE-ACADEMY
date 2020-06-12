72e974127b2bceb461428dcf92f4ee498287fe1b
Javkhaa00 <liplicker0423@gmail.com>
2020-06-12 18:44:35 +0800

    feat: hmnnnnnn

--- functions/database.js
+++ functions/database.js
@@ -1,4 +1,41 @@
-// const admin = require('firebase-admin');
-// admin.initializeApp();
+const admin = require('firebase-admin');
 
-// let db = admin.firestore();
+admin.initializeApp();
+
+const db = admin.firestore();
+
+const checkUsernames = (req, res) => {
+
+    if (req.method !== 'GET') {
+        return res.status(405).send(`${req.method} is not allowed. Use GET.`);
+    }
+
+    if (!req.query.hasOwnProperty('username')) {
+        return res
+            .status(400)
+            .send('No username provided.');
+    }
+
+    // Source: https://stackoverflow.com/a/52850529/2758318
+    const isValidDocId = id => id
+        && /^(?!\.\.?$)(?!.*__.*__)([^/]{1,1500})$/
+            .test(id);
+
+    // Document Ids should be non-empty strings
+    if (!isValidDocId(req.query.username)) {
+        return res.status(400).send('Invalid username string.');
+    }
+
+    db
+        .collection('users')
+        .doc(req.query.id)
+        .get()
+        .then(doc => {
+            /** If doc exists, the username is unavailable */
+            return res.status(200).send(!doc.exists);
+        })
+        .catch(error => handleError(req, res));
+};
+
+
+module.exports = checkUsernames;
--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,20 @@
-const functions = require('firebase-functions')
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const checkUsernames = require('./database');
+admin.initializeApp();
 
-console.log('wtf')
+let db = admin.firestore();
+
+const uid = admin.firestore().collection("tmp").doc().id
+// db.collection('users').get()
+//     .then((res) => {
+//         console.log(res)
+//     })
 
 exports.app = functions.https.onRequest((req, res) => {
-  res.send('hello from nomt')
-})
+    res.send('hello from nomt')
+});
+
+// module.exports = {
+//     checkUsernames: functions.https.onRequest(checkUsernames),
+// };
